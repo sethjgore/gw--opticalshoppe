ab9e26db3ffe2fc6b47b98da27fc1e644c9fdf8b
sethjgore <sethjgore@gmail.com>
2014-04-03 10:58:30 -0500

    added gallery component && updated header logo box height

--- grunt-tasks/options/concat.js
+++ grunt-tasks/options/concat.js
@@ -9,9 +9,9 @@ var config = require('../config');
 module.exports = {
   sass: {
     src: [
-          'resources/sass/style-prefixed.css',
           'resources/sass/plugins/owl.carousel.css',
-          'resources/sass/plugins/owl.theme.css'
+          'resources/sass/plugins/owl.theme.css',
+          'resources/sass/style-prefixed.css',
           ],
     dest: 'resources/style-compiled.css'
   }
